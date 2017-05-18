a4aa1977763e98bbc1865c6d4e54428e422e845d
dschlueter0420 <dschlueter0420@stu.neisd.net>
2017-05-18 07:07:35 -0500

    Working on 3d rotation

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws InterruptedException{
 		
-		char[][] graph  = new char[20][20];
-		View     view   = new View(20,20,10,new Point(10,10,0));
-		Point[]  points = {new Point(5, 5, 3)};
+		char[][] graph  = new char[21][21];
+		View     view   = new View(20,20,2,new Point(0,0,0));
+		Point[]  points = {new Point(-2, -2, 5),new Point(2, 2, 5),new Point(2, -2, 5),new Point(-2, 2, 5),new Point(-2, -2, 10),new Point(2, 2, 10),new Point(2, -2, 10),new Point(-2, 2, 10)};
 		Mesh     mesh   = new Mesh(points);
 
 		mesh.meshTo2D(view);
 		
-		
-		
-		for(int y=0; y<graph.length; y++){
-			for(int x=0; x<graph[0].length; x++){
-				System.out.print(graph[y][x] + " ");
+		while(true){
+			for(int y=0; y<graph.length; y++){
+				for(int x=0; x<graph[0].length; x++){
+					graph[y][x] = '.';
+					if(mesh.pOnMesh(x-10,y-10)){
+						graph[y][x] = '#';
+					}
+				}
 			}
-			System.out.println();
+			
+			for(int y=0; y<graph.length; y++){
+				for(int x=0; x<graph[0].length; x++){
+					System.out.print(graph[y][x] + " ");
+				}
+				System.out.println();
+			}
+			
+			Thread.sleep(1000);
+			
+			mesh.rotate(0, Math.PI/3, 0);
+			mesh.meshTo2D(view);
 		}
 		
 	}
--- src/Mesh.java
+++ src/Mesh.java
@@ -13,6 +13,35 @@ public class Mesh {
 		}
 	}
 	
+	public boolean pOnMesh(int x, int y){
+		for(Point point: points){
+			if(point.x == x && point.y == y){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void rotate(double rX, double rY, double rZ){
+		for(Point point: points){
+			// Rotate on x-axis
+			if(rX > 0){
+				point.y = (int) ((point.y*Math.cos(rX)) - (point.z*Math.sin(rX)));
+				point.z = (int) ((point.y*Math.sin(rX)) + (point.z*Math.cos(rX)));
+			}
+			// Rotate on y-axis
+			if(rY > 0){
+				point.z = (int) ((point.z*Math.cos(rY)) - (point.z*Math.sin(rY)));
+				point.x = (int) ((point.x*Math.sin(rY)) + (point.x*Math.cos(rY)));
+			}
+			// Rotate on z-axis
+			if(rZ > 0){
+				point.x = (int) ((point.x*Math.cos(rZ)) - (point.x*Math.sin(rZ)));
+				point.y = (int) ((point.y*Math.sin(rZ)) + (point.z*Math.cos(rZ)));
+			}
+		}
+	}
+	
 	public Point[] getPoints(){
 		return points;
 	}
